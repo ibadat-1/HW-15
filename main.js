04fced219351738933a402b676e58605bd162a62
ibadat <abdurasu10vaibd@gmail.com>
2024-07-28 20:34:32 +0300

    hw19f

---
+++ main.js
@@ -0,0 +1,203 @@
+// function addNumder (x, y) {
+//   for (let i = x; i < y; i++) console.log(i);
+// }
+//  addNumder (0, 100);
+
+
+
+
+// function addNumder1(x, y) {
+//   for (let i = x; i <= y; i++){
+//      if (i % 2 === 0){
+//         console.log(i);
+//      }
+//   }
+// }
+// addNumder1 (0, 100)
+
+
+
+
+
+// function addNumder2 (x, y) {
+//   let res = 0;
+//   for (let str = x; x < y; x++) {
+//     if (str % 2 === 0) {
+//       res = res + str;
+//     }
+//   }
+//   return res;
+// }
+// const result = addNumder2(4, 50);
+// console.log(result);
+
+
+
+
+
+// function addNumder3 (Bill, Gates) {
+//     for(let num = Bill.length - 1; num >= 0; num--){
+//         let add = Bill.charAt(num);
+//         if(add === Gates) {
+//             console.log(true);
+//         }
+//         else{
+//             console.log(false);
+
+//         }
+//     }
+
+// }
+// addNumder3("gates", "e");
+
+
+
+
+
+// function addNumder4(top) {
+//     let string = ''
+//   for (let count = top.length; count >= 0;count--) {
+//     string=string+top
+//      console.log(top[count]);
+//   }
+// }
+// addNumder4("hello");
+
+
+
+
+
+//   const top1 = (x, y) => {
+
+//   for (let i = x; i < y; i++) console.log(i);
+// }
+//   top1 (0, 100);
+
+
+
+
+
+// const getadd = (x, y) => {
+//   for (let i = x; i <= y; i++){
+//      if (i % 2 === 0){
+//         console.log(i);
+//      }
+//   }
+// }
+// getadd(0, 100)
+
+
+
+
+
+// const wor = (x, y) => {
+//   let res = 0;
+//   for (let str = x; x < y; x++) {
+//     if (str % 2 === 0) {
+//       res = res + str;
+//     }
+//   }
+//   return res;
+// }
+// const result = wor(4, 50);
+// console.log(result);
+
+
+
+
+
+// const number = (Bill, Gates) => {
+//     for (let num = Bill.length - 1; num >= 0; num--){
+//         let add = Bill.charAt(num);
+//         if(add === Gates) {
+//             console.log(true);
+//         }
+//         else{
+//             console.log(false);
+
+//         }
+//     }
+
+// }
+// number("gates", "e");
+
+
+
+
+
+// const false1 = (top) => {
+//     let string = ''
+//   for (let count = top.length; count >= 0; count--) {
+//     string = string+top
+//      console.log(top[count]);
+//   }
+// }
+// false1("hello");
+
+
+
+
+
+// const top1 = function  (x, y) {
+
+//     for (let i = x; i < y; i++) console.log(i);
+//   }
+//     top1(0, 100);
+
+
+// const getadd= function (x, y) {
+//   for (let i = x; i <= y; i++){
+//      if(i % 2 === 0 ){
+//         console.log(i);
+//      }
+//   }
+// }
+// getadd(0, 100)
+
+
+
+
+
+
+// const wor = function(x, y) {
+//   let res = 0;
+//   for (let str = x; x < y; x++) {
+//     if (str % 2 === 0) {
+//       res = res + str;
+//     }
+//   }
+//   return res;
+// }
+// const result = wor(4, 50);
+// console.log(result);
+
+
+
+
+// const number= function(Bill, Gates) {
+//     for(let num = Bill.length - 1; num >= 0; num--){
+//         let add = Bill.charAt(num);
+//         if(add === Gates) {
+//             console.log(true);
+//         }
+//         else{
+//             console.log(false);
+
+//         }
+//     }
+
+// }
+
+
+
+
+
+
+// const false1= function(top) {
+//     let string = ''
+//   for (let count = top.length; count >= 0; count--) {
+//     string = string + top
+//      console.log(top[count]);
+//   }
+// }
+// false1("hello");
